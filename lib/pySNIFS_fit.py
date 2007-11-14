49d3b9add1083d5af61ac23a79aee1af5f7037db
cbuton <cbuton>
2007-11-14 16:34:23 +0000

    (fnnls): replace any(x) by x.any().

--- lib/pySNIFS_fit.py
+++ lib/pySNIFS_fit.py
@@ -858,127 +858,126 @@ def fit_spectrum(spec,func='gaus1D',param=None,bounds=None,abs=False):
 
 
 def fnnls(XtX, Xty, tol = 0) : 
-#FNNLS Non-negative least-squares. 
-# 
-# Adapted from NNLS of Mathworks, Inc. 
-#          [x,w] = nnls(X, y) 
-# 
-# x, w = fnnls(XtX,Xty) returns the vector X that solves x = pinv(XtX)*Xty 
-# in a least squares sense, subject to x >= 0. 
-# Differently stated it solves the problem min ||y - Xx|| if 
-# XtX = X'*X and Xty = X'*y. 
-# 
-# A default tolerance of TOL = MAX(SIZE(XtX)) * NORM(XtX,1) * EPS 
-# is used for deciding when elements of x are less than zero. 
-# This can be overridden with x = fnnls(XtX,Xty,TOL). 
-# 
-# [x,w] = fnnls(XtX,Xty) also returns dual vector w where 
-# w(i) < 0 where x(i) = 0 and w(i) = 0 where x(i) > 0. 
-# 
-# See also NNLS and FNNLSb 
+    #FNNLS Non-negative least-squares. 
+    # 
+    # Adapted from NNLS of Mathworks, Inc. 
+    #          [x,w] = nnls(X, y) 
+    # 
+    # x, w = fnnls(XtX,Xty) returns the vector X that solves x = pinv(XtX)*Xty 
+    # in a least squares sense, subject to x >= 0. 
+    # Differently stated it solves the problem min ||y - Xx|| if 
+    # XtX = X'*X and Xty = X'*y. 
+    # 
+    # A default tolerance of TOL = MAX(SIZE(XtX)) * NORM(XtX,1) * EPS 
+    # is used for deciding when elements of x are less than zero. 
+    # This can be overridden with x = fnnls(XtX,Xty,TOL). 
+    # 
+    # [x,w] = fnnls(XtX,Xty) also returns dual vector w where 
+    # w(i) < 0 where x(i) = 0 and w(i) = 0 where x(i) > 0. 
+    # 
+    # See also NNLS and FNNLSb 
+     
+    # L. Shure 5-8-87 
+    # Revised, 12-15-88,8-31-89 LS. 
+    # (Partly) Copyright (c) 1984-94 by The MathWorks, Inc. 
+     
+    # Modified by R. Bro 5-7-96 according to 
+    #       Bro R., de Jong S., Journal of Chemometrics, 1997, 11, 393-401 
+    # Corresponds to the FNNLSa algorithm in the paper 
+    # 
+    # Rasmus bro 
+    # Chemometrics Group, Food Technology 
+    # Dept. Dairy and Food Science 
+    # Royal Vet. & Agricultural 
+    # DK-1958 Frederiksberg C 
+    # Denmark 
+    # rb@... 
+    # http://newton.foodsci.kvl.dk/users/rasmus.html 
+    #  Reference: 
+    #  Lawson and Hanson, "Solving Least Squares Problems", Prentice-Hall, 1974. 
+    # 
+     
+    # initialize variables
+    m,n = XtX.shape
  
-# L. Shure 5-8-87 
-# Revised, 12-15-88,8-31-89 LS. 
-# (Partly) Copyright (c) 1984-94 by The MathWorks, Inc. 
- 
-# Modified by R. Bro 5-7-96 according to 
-#       Bro R., de Jong S., Journal of Chemometrics, 1997, 11, 393-401 
-# Corresponds to the FNNLSa algorithm in the paper 
-# 
-# Rasmus bro 
-# Chemometrics Group, Food Technology 
-# Dept. Dairy and Food Science 
-# Royal Vet. & Agricultural 
-# DK-1958 Frederiksberg C 
-# Denmark 
-# rb@... 
-# http://newton.foodsci.kvl.dk/users/rasmus.html 
-#  Reference: 
-#  Lawson and Hanson, "Solving Least Squares Problems", Prentice-Hall, 1974. 
-# 
- 
-# initialize variables
-  m = XtX.shape[0]
-  n = XtX.shape[1] 
- 
-  if tol == 0 : 
-    eps = 2.2204e-16 
-    tol = 10 * eps * la.norm(XtX,1)*max(m, n)
-  #end 
- 
-  P = numpy.zeros(n, 'i') 
-  P[:] = -1 
-  Z = numpy.arange(0,n) 
- 
-  z = numpy.zeros(m, 'd') 
-  x = numpy.array(P) 
-  ZZ = numpy.array(Z) 
- 
-  w = Xty - numpy.dot(XtX, x) 
- 
-  # set up iteration criterion 
-  iter = 0 
-  itmax = 30 * n 
-
-  # outer loop to put variables into set to hold positive coefficients 
-
-  def find(X):
-      return numpy.where(X)[0]
-
-  while any(Z) and any(w[ZZ] > tol) : 
-    wt = w[ZZ].max() 
-    t = find(w[ZZ] == wt) 
-    t = t[-1:][0] 
-    t = ZZ[t] 
-    P[t] = t 
-    Z[t] = -1 
-    PP = find(P != -1) 
- 
-    ZZ = find(Z != -1) 
-    if len(PP) == 1 : 
-      XtyPP = Xty[PP] 
-      XtXPP = XtX[PP, PP] 
-      z[PP] = XtyPP / XtXPP 
-    else : 
-      XtyPP = numpy.array(Xty[PP]) 
-      XtXPP = numpy.array(XtX[PP, numpy.array(PP)[:, numpy.newaxis]]) 
-      z[PP] = numpy.dot(XtyPP, la.pinv(XtXPP)) 
+    if tol == 0 : 
+        eps = 2.2204e-16 
+        tol = 10 * eps * la.norm(XtX,1)*max(m, n)
     #end 
-    z[ZZ] = 0 
  
-# inner loop to remove elements from the positive set which no longer belong 
-    while any(z[PP] <= tol) and (iter < itmax) : 
-      iter += 1
-      iztol = find(z <= tol)
-      ip = find(P[iztol] != -1)
-      QQ = iztol[ip]
+    P = numpy.zeros(n, 'i') 
+    P -= 1
+    Z = numpy.arange(0,n) 
  
-      if len(QQ) == 1 : alpha = x[QQ] / (x[QQ] - z[QQ]) 
-      else : 
-        x_xz = x[QQ] / (x[QQ] - z[QQ])
-        alpha = x_xz.min()
+    z = numpy.zeros(m, 'd') 
+    x = P.copy()
+    ZZ = Z.copy()
  
-      x += alpha * (z - x) 
-      iabs = find(abs(x) < tol) 
-      ip = find(P[iabs] != -1) 
-      ij = iabs[ip] 
- 
-      Z[ij] = numpy.array(ij) 
-      P[ij] = -1 
-      PP = find(P != -1) 
-      ZZ = find(Z != -1) 
- 
-      if len(PP) == 1 : 
-        XtyPP = Xty[PP] 
-        XtXPP = XtX[PP, PP] 
-        z[PP] = XtyPP / XtXPP 
-      else : 
-        XtyPP = numpy.array(Xty[PP]) 
-        XtXPP = numpy.array(XtX[PP, numpy.array(PP)[:, numpy.newaxis]])
-        z[PP] = numpy.dot(XtyPP, la.pinv(XtXPP)) 
-      #endif 
-      z[ZZ] = 0 
-    x = numpy.array(z) 
     w = Xty - numpy.dot(XtX, x) 
  
-  return x, w 
+    # set up iteration criterion 
+    iter = 0 
+    itmax = 30 * n 
+
+    # outer loop to put variables into set to hold positive coefficients 
+
+    def find(X): return numpy.where(X)[0]
+
+    while Z.any() and (w[ZZ] > tol).any() : 
+        wt = w[ZZ].max() 
+        t = find(w[ZZ] == wt) 
+        t = t[-1:][0] 
+        t = ZZ[t] 
+        P[t] = t 
+        Z[t] = -1 
+        PP = find(P != -1) 
+        
+        ZZ = find(Z != -1) 
+        if len(PP) == 1 : 
+            XtyPP = Xty[PP] 
+            XtXPP = XtX[PP, PP] 
+            z[PP] = XtyPP / XtXPP 
+        else : 
+            XtyPP = numpy.array(Xty[PP]) 
+            XtXPP = numpy.array(XtX[PP, numpy.array(PP)[:, numpy.newaxis]]) 
+            z[PP] = numpy.dot(XtyPP, la.pinv(XtXPP)) 
+        #end 
+        z[ZZ] = 0 
+
+        # inner loop to remove elements from the positive set which no longer belong 
+        while (z[PP] <= tol).any() and (iter < itmax) : 
+            iter += 1
+            iztol = find(z <= tol)
+            ip = find(P[iztol] != -1)
+            QQ = iztol[ip]
+
+            if len(QQ) == 1 :
+                alpha = x[QQ] / (x[QQ] - z[QQ]) 
+            else : 
+                x_xz = x[QQ] / (x[QQ] - z[QQ])
+                alpha = x_xz.min()
+
+            x += alpha * (z - x) 
+            iabs = find(abs(x) < tol) 
+            ip = find(P[iabs] != -1) 
+            ij = iabs[ip] 
+
+            Z[ij] = numpy.array(ij) 
+            P[ij] = -1 
+            PP = find(P != -1) 
+            ZZ = find(Z != -1) 
+
+            if len(PP) == 1 : 
+                XtyPP = Xty[PP] 
+                XtXPP = XtX[PP, PP] 
+                z[PP] = XtyPP / XtXPP 
+            else : 
+                XtyPP = numpy.array(Xty[PP]) 
+                XtXPP = numpy.array(XtX[PP, numpy.array(PP)[:, numpy.newaxis]])
+                z[PP] = numpy.dot(XtyPP, la.pinv(XtXPP)) 
+            #endif 
+            z[ZZ] = 0 
+        x = numpy.array(z) 
+        w = Xty - numpy.dot(XtX, x) 
+ 
+    return x, w 
