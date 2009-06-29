60ad685d07c42b7ec19578219385e87d50f521de
ycopin <ycopin>
2009-06-29 18:04:15 +0000

    remove call to S.degrees(), not available for python-2.4.

--- apps/extract_star/extract_star.py
+++ apps/extract_star/extract_star.py
@@ -861,7 +861,7 @@ if __name__ == "__main__":
     print_msg("  Reference position guess [%.0fA]: %.2f x %.2f spx" % \
               (lref,xc,yc), 1)
     print_msg("  ADR guess: delta=%.2f, theta=%.1f deg" % \
-              (delta0, S.degrees(theta0)), 1)
+              (delta0, theta0/S.pi*180), 1) # S.degrees() from python-2.5 only
 
     # 3) Other parameters
     PA       = S.median(PA_vec[good])
