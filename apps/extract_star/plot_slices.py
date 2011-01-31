f66952e0ba873f97d8c46cd94be5827d7beeffdd
ycopin <ycopin>
2011-01-31 11:35:08 +0000

    new option '-s/--stack' to display stacked spectra instead of mean spectrum.

--- apps/extract_star/plot_slices.py
+++ apps/extract_star/plot_slices.py
@@ -10,151 +10,160 @@ from ToolBox.MPL import get_backend
 __author__ = "Yannick Copin <y.copin@ipnl.in2p3.fr>"
 __version__ = '$Id$'
 
-if __name__=='__main__':
+parser = optparse.OptionParser(version=__version__)
 
-    parser = optparse.OptionParser(version=__version__)
+parser.add_option("-N", "--nmeta", type='int',
+                  help="Number of meta-slices [%default].",
+                  default=12)
+parser.add_option("-r", "--range", type='string',
+                  help="Flux range in percentile [%default].",
+                  default='3,97')
+parser.add_option("-R", "--rangePerSlice", action='store_true',
+                  help="Compute flux range per slice.",
+                  default=False)
+parser.add_option("-S", "--spatialStats", action='store_true',
+                  help="Compute spatial statistics.",
+                  default=False)
+parser.add_option("-g", "--graph", type="string",
+                  help="Graphic output format [%default]",
+                  default='pylab')
+parser.add_option("-V", "--variance", action='store_true',
+                  help="Plot variance rather than signal.",
+                  default=False)
+parser.add_option("-s", "--stack", action='store_true',
+                  help="Plot stacked spectra.",
+                  default=False)
 
-    parser.add_option("-N", "--nmeta", type='int',
-                      help="Number of meta-slices [%default].",
-                      default=12)
-    parser.add_option("-r", "--range", type='string',
-                      help="Flux range in percentile [%default].",
-                      default='3,97')
-    parser.add_option("-R", "--rangePerSlice", action='store_true',
-                      help="Compute flux range per slice.",
-                      default=False)
-    parser.add_option("-S", "--spatialStats", action='store_true',
-                      help="Compute spatial statistics.",
-                      default=False)
-    parser.add_option("-g", "--graph", type="string",
-                      help="Graphic output format [%default]",
-                      default='pylab')
-    parser.add_option("-V", "--variance", action='store_true',
-                      help="Plot variance rather than signal.",
-                      default=False)
+opts,args = parser.parse_args()
+inname = args[0]
+basename,ext = os.path.splitext(os.path.basename(args[0]))
 
-    opts,args = parser.parse_args()
-    inname = args[0]
-    basename,ext = os.path.splitext(os.path.basename(args[0]))
+try:
+    fmin,fmax = [ float(t) for t in opts.range.split(',') ]
+    assert 0<=fmin<100 and 0<fmax<=100 and fmin<fmax
+except (ValueError,AssertionError):
+    parser.error("invalid option '-r/--range %s'" % opts.range)
 
-    try:
-        fmin,fmax = [ float(t) for t in opts.range.split(',') ]
-        assert 0<=fmin<100 and 0<fmax<=100 and fmin<fmax
-    except (ValueError,AssertionError):
-        parser.error("invalid option '-r/--range %s'" % opts.range)
+try:                                # Try to read a Euro3D cube
+    fcube = pySNIFS.SNIFS_cube(e3d_file=inname)
+    isE3D = True
+except ValueError:                  # Try to read a 3D FITS cube
+    fcube = pySNIFS.SNIFS_cube(fits3d_file=inname)
+    isE3D = False
+print "Cube %s: %d slices [%.2f-%.2f], %d spaxels" % \
+    (basename, fcube.nslice, fcube.lstart, fcube.lend, fcube.nlens)
 
-    try:                                # Try to read a Euro3D cube
-        fcube = pySNIFS.SNIFS_cube(e3d_file=inname)
-        isE3D = True
-    except ValueError:                  # Try to read a 3D FITS cube
-        fcube = pySNIFS.SNIFS_cube(fits3d_file=inname)
-        isE3D = False
-    print "Cube %s: %d slices [%.2f-%.2f], %d spaxels" % \
-        (basename, fcube.nslice, fcube.lstart, fcube.lend, fcube.nlens)
+# Meta-slice definition (min,max,step [px])
+imin = 10                           # 1st slice [px]
+imax = fcube.nslice - 10            # Last slice [px]
+istep = (imax-imin)//opts.nmeta     # Metaslice thickness [px]
+imax = imin + opts.nmeta*istep
+slices = [imin,imax,istep]
 
-    # Meta-slice definition (min,max,step [px])
-    imin = 10                           # 1st slice [px]
-    imax = fcube.nslice - 10            # Last slice [px]
-    istep = (imax-imin)//opts.nmeta     # Metaslice thickness [px]
-    imax = imin + opts.nmeta*istep
-    slices = [imin,imax,istep]
+if isE3D:
+    cube = pySNIFS.SNIFS_cube(e3d_file=inname, slices=slices)
+else:
+    cube = pySNIFS.SNIFS_cube(fits3d_file=inname, slices=slices)
+cube.x = cube.i - 7                 # From arcsec to spx
+cube.y = cube.j - 7
 
-    if isE3D:
-        cube = pySNIFS.SNIFS_cube(e3d_file=inname, slices=slices)
+lbounds = fcube.lbda[range(imin,imax+1,istep)]
+assert len(lbounds)==len(cube.lbda)+1
+
+import matplotlib as M
+backend,figname = get_backend(opts.graph, name='slices_%s' % basename)
+if backend:
+    M.use(backend)
+import pylab as P
+
+fig = P.figure(figsize=(10,8))
+fig.subplots_adjust(left=0.06, bottom=0.25, top=0.95,
+                    hspace=0.03, wspace=0.03)
+
+objname = fcube.e3d_data_header.get("OBJECT", 'unknown')
+fig.text(0.5, 0.97, 
+         u"%s [%s], slices of %.0f Å" % (basename, objname,
+                                         fcube.lstep*istep),
+         fontsize='large', ha='center', va='center')
+
+ncol = int(P.floor(P.sqrt(cube.nslice)))
+nrow = int(P.ceil(cube.nslice/float(ncol)))
+extent = (cube.x.min()-0.5,cube.x.max()+0.5,
+          cube.y.min()-0.5,cube.y.max()+0.5)
+
+if not opts.rangePerSlice:
+    if opts.variance:
+        data = cube.var
     else:
-        cube = pySNIFS.SNIFS_cube(fits3d_file=inname, slices=slices)
-    cube.x = cube.i - 7                 # From arcsec to spx
-    cube.y = cube.j - 7
+        data = cube.data
+    vmin,vmax = P.prctile(data[P.isfinite(cube.var)], (fmin,fmax))
+    print "%s range [%.0f-%.0f%%]: %g,%g" % \
+        (opts.variance and 'Variance' or 'Flux', fmin,fmax,vmin,vmax)
+    fig.subplots_adjust(right=0.90)
+else:
+    fig.subplots_adjust(right=0.95)
 
-    lbounds = fcube.lbda[range(imin,imax+1,istep)]
-    assert len(lbounds)==len(cube.lbda)+1
-
-    import matplotlib as M
-    backend,figname = get_backend(opts.graph, name='slices_%s' % basename)
-    if backend:
-        M.use(backend)
-    import pylab as P
-
-    fig = P.figure(figsize=(10,8))
-    fig.subplots_adjust(left=0.06, bottom=0.25, top=0.95,
-                        hspace=0.03, wspace=0.03)
-
-    objname = fcube.e3d_data_header.get("OBJECT", 'unknown')
-    fig.text(0.5, 0.97, 
-             u"%s [%s], slices of %.0f Å" % (basename, objname,
-                                             fcube.lstep*istep),
-             fontsize='large', ha='center', va='center')
-
-    ncol = int(P.floor(P.sqrt(cube.nslice)))
-    nrow = int(P.ceil(cube.nslice/float(ncol)))
-    extent = (cube.x.min()-0.5,cube.x.max()+0.5,
-              cube.y.min()-0.5,cube.y.max()+0.5)
-
-    if not opts.rangePerSlice:
-        if opts.variance:
-            data = cube.var
-        else:
-            data = cube.data
-        vmin,vmax = P.prctile(data[P.isfinite(cube.var)], (fmin,fmax))
-        print "%s range [%.0f-%.0f%%]: %g,%g" % \
-            (opts.variance and 'Variance' or 'Flux', fmin,fmax,vmin,vmax)
-        fig.subplots_adjust(right=0.90)
+# Loop over slices
+for i in xrange(cube.nslice):        # Loop over meta-slices
+    ax = fig.add_subplot(ncol, nrow, i+1, aspect='equal')
+    if opts.variance:
+        data = cube.slice2d(i, coord='p', var=True)
     else:
-        fig.subplots_adjust(right=0.95)
+        data = cube.slice2d(i, coord='p')
 
+    gdata = data[N.isfinite(data)] # Non-NaN values
+    m,s = gdata.mean(),gdata.std()
+    print "Slice #%02d/%d, %.0f Å [%.0f-%.0f]: " \
+        "mean=%g, stddev=%g (%.2f%%)" % \
+        (i+1,cube.nslice,cube.lbda[i],lbounds[i],lbounds[i+1],
+         m,s,s/m*100)
 
-    # Loop over slices
-    for i in xrange(cube.nslice):        # Loop over meta-slices
-        ax = fig.add_subplot(ncol, nrow, i+1, aspect='equal')
-        if opts.variance:
-            data = cube.slice2d(i, coord='p', var=True)
-        else:
-            data = cube.slice2d(i, coord='p')
+    if opts.rangePerSlice:
+        var = cube.slice2d(i, coord='p', var=True)
+        vmin,vmax = P.prctile(data[P.isfinite(var)], (fmin,fmax))
 
-        gdata = data[N.isfinite(data)] # Non-NaN values
-        m,s = gdata.mean(),gdata.std()
-        print "Slice #%02d/%d, %.0f Å [%.0f-%.0f]: " \
-            "mean=%g, stddev=%g (%.2f%%)" % \
-            (i+1,cube.nslice,cube.lbda[i],lbounds[i],lbounds[i+1],
-             m,s,s/m*100)
+    im = ax.imshow(data,
+                   origin='lower', extent=extent, interpolation='nearest',
+                   vmin=vmin, vmax=vmax, cmap=M.cm.jet)
 
-        if opts.rangePerSlice:
-            var = cube.slice2d(i, coord='p', var=True)
-            vmin,vmax = P.prctile(data[P.isfinite(var)], (fmin,fmax))
-        
-        im = ax.imshow(data,
-                       origin='lower', extent=extent, interpolation='nearest',
-                       vmin=vmin, vmax=vmax, cmap=M.cm.jet)
+    lbl = u"%.0f Å [%.0f-%.0f]" % (cube.lbda[i],lbounds[i],lbounds[i+1])
+    if opts.spatialStats:
+        lbl += "\nRMS=%.2f%%" % (s/m*100)
+    ax.text(0.1,0.1, lbl, 
+            fontsize=9, horizontalalignment='left', 
+            transform=ax.transAxes)
+    ax.axis(extent)
 
-        lbl = u"%.0f Å [%.0f-%.0f]" % (cube.lbda[i],lbounds[i],lbounds[i+1])
-        if opts.spatialStats:
-            lbl += "\nRMS=%.2f%%" % (s/m*100)
-        ax.text(0.1,0.1, lbl, 
-                fontsize=9, horizontalalignment='left', 
-                transform=ax.transAxes)
-        ax.axis(extent)
+    # Axis
+    P.setp(ax.get_xticklabels()+ax.get_yticklabels(), fontsize=8)
+    if ax.is_last_row() and ax.is_first_col():
+        ax.set_xlabel("X [spx]", fontsize=8)
+        ax.set_ylabel("Y [spx]", fontsize=8)
+    if not ax.is_last_row():
+        P.setp(ax.get_xticklabels(), visible=False)
+    if not ax.is_first_col():
+        P.setp(ax.get_yticklabels(), visible=False)
 
-        # Axis
-        P.setp(ax.get_xticklabels()+ax.get_yticklabels(), fontsize=8)
-        if ax.is_last_row() and ax.is_first_col():
-            ax.set_xlabel("X [spx]", fontsize=8)
-            ax.set_ylabel("Y [spx]", fontsize=8)
-        if not ax.is_last_row():
-            P.setp(ax.get_xticklabels(), visible=False)
-        if not ax.is_first_col():
-            P.setp(ax.get_yticklabels(), visible=False)
+# Colorbar
+if not opts.rangePerSlice:
+    cax = fig.add_axes([0.92,0.25,0.02,0.7])
+    cbar = fig.colorbar(im, cax, orientation='vertical')
 
-    # Colorbar
-    if not opts.rangePerSlice:
-        cax = fig.add_axes([0.92,0.25,0.02,0.7])
-        cbar = fig.colorbar(im, cax, orientation='vertical')
+ax2 = fig.add_axes([0.06,0.06,0.88,0.15])
 
-    # Spectrum
+if opts.stack:                  # Stacked spectra
+    vmin,vmax = P.prctile(fcube.data[P.isfinite(fcube.var)], (fmin,fmax))
+    ax2.imshow(fcube.data.T, vmin=vmin, vmax=vmax,
+               extent=(fcube.lstart,fcube.lend,0,fcube.nlens-1))
+    ax2.set_aspect(3, adjustable='box')
+    ax2.set_xlabel(u"Wavelength [Å]", fontsize=8)
+    ax2.set_ylabel("Spx #", fontsize=8)
+    P.setp(ax2.get_xticklabels()+ax2.get_yticklabels(), fontsize=8)
+
+else:                           # Mean spectrum
     spec = fcube.data.mean(axis=1)
     dspec = P.sqrt(fcube.var.sum(axis=1)/fcube.nlens**2)
-    
-    ax2 = fig.add_axes([0.06,0.06,0.88,0.15])
-    
+
     ax2.plot(fcube.lbda, spec, 'b-')
     xp,yp = M.mlab.poly_between(fcube.lbda, spec-dspec, spec+dspec)
     ax2.fill(xp,yp, fc='b', ec='b', alpha=0.3)
@@ -162,7 +171,7 @@ if __name__=='__main__':
     ax2.errorbar(cube.lbda, cube.data.mean(axis=1),
                  yerr=P.sqrt(cube.var.sum(axis=1))/cube.nlens,
                  fmt='go')
-    
+
     ax2.set_xlim(fcube.lstart,fcube.lend)
     ax2.set_xlabel(u"Wavelength [Å]", fontsize=8)
     if opts.variance:
@@ -176,13 +185,13 @@ if __name__=='__main__':
             fxlabel += u"²"
     ax2.set_ylabel(fxlabel, fontsize=8)
     P.setp(ax2.get_xticklabels()+ax2.get_yticklabels(), fontsize=8)
-    
-    # Metaslice boundaries
-    for l in lbounds:
-        ax2.axvline(l, c='0.8', zorder=0)
-                
-    if backend:
-        print "Saving plot in", figname
-        fig.savefig(figname)
-    else:
-        P.show()
+
+# Metaslice boundaries
+for l in lbounds:
+    ax2.axvline(l, c='0.8', zorder=0)
+
+if backend:
+    print "Saving plot in", figname
+    fig.savefig(figname)
+else:
+    P.show()
